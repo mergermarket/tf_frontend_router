825d21db4f262f1d3fa608a7f0d84bc22667df5c
mikemey <miguel.maestro@gmx.co.uk>
2017-07-21 10:26:16 +0100

    CRUB-92: added timeout variables. :

--- fastly.tf
+++ fastly.tf
@@ -1,10 +1,13 @@
 module "fastly" {
   source = "github.com/mergermarket/tf_fastly_frontend"
 
-  domain_name       = "${var.fastly_domain}"
-  backend_address   = "${module.alb.alb_dns_name}"
-  env               = "${var.env}"
-  caching           = "${var.fastly_caching}"
-  ssl_cert_check    = "${var.ssl_cert_check}"
-  ssl_cert_hostname = "${var.ssl_cert_hostname}"
+  domain_name           = "${var.fastly_domain}"
+  backend_address       = "${module.alb.alb_dns_name}"
+  env                   = "${var.env}"
+  caching               = "${var.fastly_caching}"
+  ssl_cert_check        = "${var.ssl_cert_check}"
+  ssl_cert_hostname     = "${var.ssl_cert_hostname}"
+  connect_timeout       = "${var.connect_timeout}"
+  first_byte_timeout    = "${var.first_byte_timeout}"
+  between_bytes_timeout = "${var.between_bytes_timeout}"
 }
